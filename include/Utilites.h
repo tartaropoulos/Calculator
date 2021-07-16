0ee817c2599095d92994f12c2a124397fe255bae
tartaropoulos <rangel.stanev@gmail.com>
2021-07-16 12:38:53 +0300

    add more constraints in IsCalculatorInput concept for integral types

--- include/Utilites.h
+++ include/Utilites.h
@@ -1,17 +1,23 @@
 #ifndef UTILITES_H
 #define UTILITES_H
 
-#include "Calculator.h"
-
 #include <concepts>
 #include <iostream>
 #include <string_view>
 
+#include "Calculator.h"
+
 template<typename T>
-// concept IsCalculatorInput = std::integral<T> || std::floating_point<T> || std::same_as<T, Calculator::Operators>;
-concept IsCalculatorInput = (!std::same_as<T, char> && !std::same_as<T, bool> && std::integral<T>) || 
-                              std::floating_point<T> || 
-                              std::same_as<T, Calculator::Operators>;
+concept IsCalculatorInput = (std::is_arithmetic_v<T> || std::same_as<T, Calculator::Operators>) &&
+                            // Do exist another way to separate characters and bool from integral types?
+                            !std::same_as<T, bool> &&
+                            !std::same_as<T, char> &&
+                            !std::same_as<T, signed char> &&
+                            !std::same_as<T, unsigned char> &&
+                            !std::same_as<T, wchar_t> &&
+                            !std::same_as<T, char8_t> &&
+                            !std::same_as<T, char16_t> &&
+                            !std::same_as<T, char32_t>;
 
 bool getInputItem(std::string_view msg, IsCalculatorInput auto& value)
 {
